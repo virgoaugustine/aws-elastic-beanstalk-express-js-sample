f627958501fbde522f4ad5aac6c93374aa7fd7e6
virgoaugustine <virgoaugustine@gmail.com>
2021-06-19 13:47:59 +0000

    changed messsage

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send("Virgo's DevOps Page"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
